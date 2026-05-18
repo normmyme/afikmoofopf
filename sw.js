bb0aec2f56c40477012d17ca449556cce0b2ba3b
Norman <normmy22194@gmail.com>
2026-05-18 18:13:07 +0600

    Update service worker cache and files list

--- sw.js
+++ sw.js
@@ -1,13 +1,26 @@
-const CACHE = 'mbank-pwa-v1';
+const CACHE = 'mbank-pwa-v2';
+const BASE = 'https://normmyme.github.io/afikmoofopf/';
+
+const FILES = [
+  BASE + 'payment.html',
+  BASE + 'manifest.json',
+  BASE + 'icon-192.png',
+  BASE + 'icon-512.png',
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(['/payment.html']))
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
